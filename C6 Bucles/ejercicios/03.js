8848faf11ebb1d4015f96afbfcc51ab7af98c64b
BrunoGiugno26 <brunogiugno@gmail.com>
2024-10-05 15:42:46 -0300

    ejercisios bucles del 1 al 7 terminados

--- C6 Bucles/ejercicios/03.js
+++ C6 Bucles/ejercicios/03.js
@@ -3,6 +3,23 @@ function obtenerMayor(x, y) {
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
   // Tu código:
+
+  if(x > y){
+    return x
+  } else{
+    return y
+  }
+
+  //if(x > y){
+  //  return x
+  //} else if(y>x){
+  //  return y
+  //} else if (x === y || y === x){
+  //  return x || y
+  //}
+
+  //return Math.max(x,y)
 }
 
+console.log(obtenerMayor(4,2))
 module.exports = obtenerMayor;
