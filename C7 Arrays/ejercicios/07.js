3b2268aa5dc34f9ff2611d884b636f71afead14d
BrunoGiugno26 <brunogiugno@gmail.com>
2024-10-21 10:42:44 -0300

    dependencias arregladas

--- C7 Arrays/ejercicios/07.js
+++ C7 Arrays/ejercicios/07.js
@@ -1,9 +1,14 @@
-function ordenarArray(array) {
+function ordenarArray(array){
   // Ordena los elementos del areglo array de menor a mayor.
-  // Devuelve el arreglo resultante.
-  // Tu código:
+  //Devuelve el arreglo resultante.
+  //Tu código:
+
   array.sort()
   return array;
+
 }
+
+console.log(ordenarArray([6,2,3,1,4]));
+
 
 module.exports = ordenarArray;
